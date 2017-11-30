019f0da3bad4b11b802e07eaf8269d7f55fa3fb1
Forest Anderson <wizcardforest@gmail.com>
2017-11-30 17:25:27 -0500

    Fixed tilt

--- Arcane Engine Core/src/game/Player.cpp
+++ Arcane Engine Core/src/game/Player.cpp
@@ -11,7 +11,6 @@ namespace arcane { namespace game {
 		m_Orientation = glm::angleAxis(glm::radians(0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
 		m_Tilt = 0.0f;
-		m_Tilt = 0.0f;
 
 		m_MainRotor = main_rotor;
 		m_BackRotor = back_rotor;
